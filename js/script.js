fe78d87329cc59a1a8bb27b257ddd416d392320b
JHenrique <henryque.pc.game@gmail.com>
2025-06-05 15:01:49 -0300

    atualizado porjetos criado

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Portfólio carregado com sucesso!");
 
+  // ------------------------- variaveis ---
   const btn_menu = document.querySelector(".hamburguer");
   const menu = document.querySelector(".menu-mobile");
   const btn_close = document.querySelector(".menu-close");
@@ -9,7 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputEmail = document.querySelector("#email");
   const inputMensagem = document.querySelector("#mensagem");
   const camposObrigatorio = document.querySelector(".tdCampo_obr");
+  const boxProjetos = document.getElementById("list-projetos");
+  const cardProjetos = document.querySelectorAll(".list-projeto");
+  const leftArrow = document.querySelector(".left-arrow");
+  const rightArrow = document.querySelector(".right-arrow");
 
+  // ------------------------- menu hamburguer ---
   btn_menu.addEventListener("click", () => {
     menu.classList.add("active");
     btn_menu.classList.remove("active_bars");
@@ -22,6 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
     btn_close.classList.remove("active_bars");
   });
 
+  // ------------------------- enviando mensagem valdiadar formulario ---
   btn_enviar.addEventListener("click", () => {
     if (!inputEmail.value || !inputMensagem.value) {
       camposObrigatorio.innerHTML = "Preencha todos Campos é Obrigatorio *";
@@ -49,6 +56,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  // ------------------------- header scroll ---
+
   const btnTop = document.querySelector(".subir-top");
   window.addEventListener("scroll", () => {
     if (window.scrollY > 750) {
@@ -57,10 +66,73 @@ document.addEventListener("DOMContentLoaded", () => {
       btnTop.classList.remove("active-scroll");
     }
   });
+
+  // ------------------------- scroll top ---
   btnTop.addEventListener("click", () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
+  });
+
+  // ------------------------- calculando todos os card arrow  ---
+
+  let cardWidth;
+  cardProjetos.forEach((card) => {
+    let widthValue = card.offsetWidth;
+
+    cardWidth = widthValue + 24;
+  });
+
+  let qtdCard = cardProjetos.length * cardWidth;
+
+  // ------------------------- arrow scroll left ---
+  leftArrow.addEventListener("click", () => {
+    boxProjetos.scrollLeft -= cardWidth;
+
+    if (boxProjetos.scrollLeft <= 0) {
+      boxProjetos.scrollLeft = qtdCard;
+      return;
+    }
+
+    if (window.innerWidth <= 1626) {
+      boxProjetos.scrollLeft -= cardWidth - cardWidth;
+      return;
+    }
+
+    if (window.innerWidth <= 768) {
+      boxProjetos.scrollLeft -= cardWidth - cardWidth;
+      return;
+    }
+  });
+
+  // ------------------------ arrow scroll right ---
+  rightArrow.addEventListener("click", () => {
+    boxProjetos.scrollLeft += cardWidth;
+    let scrollLeft = boxProjetos.scrollLeft;
+    console.log(scrollLeft);
+    if (
+      scrollLeft + cardWidth >= qtdCard - (cardWidth - 2) &&
+      window.innerWidth > 768
+    ) {
+      boxProjetos.scrollLeft = 0;
+      return;
+    }
+
+    if (window.innerWidth <= 1626 && window.innerWidth > 768) {
+      boxProjetos.scrollLeft += cardWidth - cardWidth;
+      return;
+    }
+
+    if (window.innerWidth <= 768) {
+      if (scrollLeft + cardWidth >= qtdCard) {
+        boxProjetos.scrollLeft = 0;
+        return;
+      } else {
+        boxProjetos.scrollLeft += cardWidth - cardWidth;
+      }
+
+      return;
+    }
   });
 });
